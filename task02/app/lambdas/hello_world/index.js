ef98857c649eca074cec141b928367d7a0a781d6
nurlan <baitassovnurlan1507@gmail.com>
2024-11-02 20:14:22 +0500

    use path

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,9 +1,8 @@
 exports.handler = async (event) => {
-    const { httpMethod, resource } = event;
-    console.log(event);
-    
+    const { httpMethod, path } = event;
+
     // Handle the /hello GET request
-    if (resource === '/hello') {
+    if (httpMethod === 'GET' && path === '/hello') {
         return {
             statusCode: 200,
             body: JSON.stringify({
@@ -18,7 +17,7 @@ exports.handler = async (event) => {
         statusCode: 400,
         body: JSON.stringify({
             statusCode: 400,
-            message: `Bad request syntax or unsupported method. Request path: ${resource}. HTTP method: ${httpMethod}`
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`
         })
     };
 };
