d05f30048ad1a1e0c03acd5a208f1c2a6a0f280c
nurlan <baitassovnurlan1507@gmail.com>
2024-11-02 19:25:44 +0500

    fix: 400 code handler

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,8 +1,19 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        message: 'Hello from Lambda',
+    const { httpMethod, path } = event;
+
+    if (httpMethod === 'GET' && path === '/hello') {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Hello from Lambda'
+            }),
+        };
+    }
+
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`
+        }),
     };
-    return response;
 };
