93cb084d1e2a3ac5245e2c79ad91b1350585785d
nurlan <baitassovnurlan1507@gmail.com>
2024-11-02 21:17:14 +0500

    feat: fixed my issue, neede to ad body

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,13 +1,12 @@
 exports.handler = async (event) => {
-
-    const path = event.rawPath
-    const method = event.requestContext.http.method
-    // Handle the /hello GET request
-    console.log(event);
+    const path = event.rawPath;
+    const method = event.requestContext.http.method;
     
+    // Logging the event and route info for debugging
+    console.log(event);
     console.log('HELLOOO');
     
-    if (event.rawPath == '/hello') {
+    if (path === '/hello') {
         console.log('HELLO THIS IS HELLO ROUTE');
         
         return {
@@ -15,8 +14,11 @@ exports.handler = async (event) => {
             headers: {
                 "Content-Type": "application/json"
             },
-            message: 'Hello from Lambda'
-        }
+            body: JSON.stringify({
+                statusCode: 200,
+                message: 'Hello from Lambda'
+            })
+        };
     }
 
     // Return a 400 error for any other request
@@ -25,6 +27,9 @@ exports.handler = async (event) => {
         headers: {
             "Content-Type": "application/json"
         },
-        message: `Bad request syntax or unsupported method. Request path: ${event.rawPath}. HTTP method: ${event.requestContext.http.method}`
-    }
+        body: JSON.stringify({
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        })
+    };
 };
