833345b9d0239c1f7f7f48c1d192bbd67ae00190
nurlan <baitassovnurlan1507@gmail.com>
2024-11-08 15:48:51 +0600

    fix: task03 fix response

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,12 +1,8 @@
 exports.handler = async (event) => {
     // TODO implement
     return {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            statusCode: 200,
-            message: 'Hello from Lambda'
-        })
-    };
+        statusCode: 200,
+        message: 'Hello from Lambda'
+    }
+
 };
