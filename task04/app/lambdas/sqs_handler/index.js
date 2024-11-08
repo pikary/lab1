bf7897ac0917b2353a2214ab690bf6195f68cd97
nurlan <baitassovnurlan1507@gmail.com>
2024-11-08 18:47:30 +0600

    feat: sns msg capture

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,7 +1,11 @@
 exports.handler = async (event) => {
-    console.log('Hello world!!!!');
-    
-    // Return a success message
+    console.log("Received event:", JSON.stringify(event, null, 2));
+
+    for (const record of event.Records) {
+        const messageBody = record.body;
+        console.log("Message Body:", messageBody);
+    }
+
     return {
         statusCode: 200,
         body: JSON.stringify('Messages processed successfully'),
