0f303909625402a04882da8d3ea9a3de215fa5e3
nurlan <baitassovnurlan1507@gmail.com>
2024-11-08 18:24:05 +0600

    fix: lambda permission

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,16 +1,6 @@
-// Lambda handler to process SQS messages and log content to CloudWatch Logs
 exports.handler = async (event) => {
-    // Log the entire event for reference
-    console.log("Received event:", JSON.stringify(event, null, 2));
-
-    // Loop through each record (SQS message) in the event
-    for (const record of event.Records) {
-        // Extract the message body
-        const messageBody = record.body;
-        // Log the message content to CloudWatch
-        console.log("Message Body:", messageBody);
-    }
-
+    console.log('Hello world!!!!');
+    
     // Return a success message
     return {
         statusCode: 200,
