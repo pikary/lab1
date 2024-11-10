5750299f49f12b72c576fc1c398621f779b56290
nurlan <baitassovnurlan1507@gmail.com>
2024-11-11 02:33:30 +0600

    s

--- task06/app/lambdas/audit_producer/index.js
+++ task06/app/lambdas/audit_producer/index.js
@@ -1,12 +1,36 @@
-const AWS = require("aws-sdk");
-
-const docClient = new AWS.DynamoDB.DocumentClient();
-const tableName = "Audit"
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const AUDIT_TABLE = "Audit-test";
 
 exports.handler = async (event) => {
-    console.log(event);
+    for (const record of event.Records) {
+        const { eventName, dynamodb: dbRecord } = record;
+        const itemKey = dbRecord.Keys.key.S;
 
-    return {
-        event
+        if (eventName === "INSERT") {
+            const newValue = AWS.DynamoDB.Converter.unmarshall(dbRecord.NewImage);
+            const auditEntry = {
+                id: uuid.v4(),
+                itemKey,
+                modificationTime: new Date().toISOString(),
+                newValue
+            };
+            await dynamodb.put({ TableName: AUDIT_TABLE, Item: auditEntry }).promise();
+        } else if (eventName === "MODIFY") {
+            const oldImage = AWS.DynamoDB.Converter.unmarshall(dbRecord.OldImage);
+            const newImage = AWS.DynamoDB.Converter.unmarshall(dbRecord.NewImage);
+            const updatedAttribute = Object.keys(newImage).find(key => newImage[key] !== oldImage[key]);
+
+            const auditEntry = {
+                id: uuid.v4(),
+                itemKey,
+                modificationTime: new Date().toISOString(),
+                updatedAttribute,
+                oldValue: oldImage[updatedAttribute],
+                newValue: newImage[updatedAttribute]
+            };
+            await dynamodb.put({ TableName: AUDIT_TABLE, Item: auditEntry }).promise();
+        }
     }
 };
