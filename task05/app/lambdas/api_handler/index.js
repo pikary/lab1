560e1424d7c231acddf707362c60721729585222
nurlan <baitassovnurlan1507@gmail.com>
2024-11-10 16:50:20 +0600

    feat: task05

---
+++ task05/app/lambdas/api_handler/index.js
@@ -0,0 +1,50 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+    DynamoDBDocumentClient,
+    PutCommand,
+} = require("@aws-sdk/lib-dynamodb");
+const { v4: uuidv4 } = require("uuid");
+
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
+const tableName = "Events";
+
+exports.handler = async (event, context) => {
+    let body;
+    let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
+
+    try {
+        const requestJSON = JSON.parse(event.body);
+        const newEvent = {
+            id: uuidv4(),
+            principalId: requestJSON.principalId,
+            createdAt: new Date().toISOString(),
+            body: requestJSON.content,
+        };
+
+        await dynamo.send(
+            new PutCommand({
+                TableName: tableName,
+                Item: newEvent,
+            })
+        );
+
+        statusCode = 201;
+        body = {
+            statusCode: 201,
+            event: newEvent,
+        };
+    } catch (err) {
+        statusCode = 400;
+        body = { error: err.message };
+    }
+
+    return {
+        statusCode,
+        body: JSON.stringify(body),
+        headers,
+    };
+};
