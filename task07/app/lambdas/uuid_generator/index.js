98797c2ee994755b49c9ae8addd0dcfe3f4391f0
nurlan <baitassovnurlan1507@gmail.com>
2024-11-11 11:50:40 +0600

    fix: minor fixes

--- task07/app/lambdas/uuid_generator/index.js
+++ task07/app/lambdas/uuid_generator/index.js
@@ -13,8 +13,7 @@ exports.handler = async (event) => {
         const fileName = `${timestamp}`; 
         // Prepare file content
         const fileContent = JSON.stringify({
-            generatedAt: timestamp,
-            uuids: ids
+            ids
         });
 
         const params = {
