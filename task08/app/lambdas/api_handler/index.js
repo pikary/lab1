84336f2075136be983ad58c04160cfe956fa98a5
Nurlan Baitassov Olegovich <baitassovnurlan1507@gmail.com>
2024-11-12 14:11:01 +0600

    testing

--- task08/app/lambdas/api_handler/index.js
+++ task08/app/lambdas/api_handler/index.js
@@ -1,12 +1,13 @@
 // const WeatherService = require('weatherapi_layer/index'); // Adjust the path as needed
-const WeatherService = require('weatherapi_layer'); 
+const WeatherService = require('/var/task/lambdas/api_handler/index.js'); 
 
 exports.handler = async (event) => {
     try {
+        console.log(WeatherService);
+        
         const weatherService = new WeatherService();
      
-        const { latitude = 52.52, longitude = 13.405 } = event.queryStringParameters || {}
-        const forecast = await weatherService.getWeatherForecast(latitude, longitude);
+        const forecast = await weatherService.getWeatherForecast();
         return {
             headers: {
                 "Content-Type": "application/json"
