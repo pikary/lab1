d63774f57696a8575ab41c4d3e79f1e921d84764
Nurlan Baitassov Olegovich <baitassovnurlan1507@gmail.com>
2024-11-12 16:27:07 +0600

    f

--- task08/app/lambdas/api_handler/index.js
+++ task08/app/lambdas/api_handler/index.js
@@ -2,14 +2,13 @@ const moment = require('moment')
 const WeatherAPI = require('nurlan')
 
 exports.handler = async (event) => {
-    try{
+    try {
         const weatherApiInstance = new WeatherAPI()
-        const result =await weatherApiInstance.getWeather()
-        return {
-            body: JSON.stringify(result)
-        }
-    }catch(e){
+        const result = await weatherApiInstance.getWeather()
+        return  JSON.stringify(result)
+
+    } catch (e) {
         console.log(e);
-        
+
     }
 };
