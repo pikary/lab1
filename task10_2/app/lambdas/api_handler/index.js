b8f6a4754efa58befa44756d35f72ad6db05ff5e
Nurlan Baitassov Olegovich <baitassovnurlan1507@gmail.com>
2024-11-14 18:36:17 +0500

    asdasdasd

--- task10_2/app/lambdas/api_handler/index.js
+++ task10_2/app/lambdas/api_handler/index.js
@@ -324,13 +324,13 @@ exports.handler = async (event) => {
             const params = {
                 TableName: 'cmtr-77278c6b-Reservations-test', 
                 Item: {
-                    id: id,
-                    tableNumber:body.tableNumber,
-                    clientName:body.clientName,
-                    phoneNumber:body.phoneNumber,
-                    date:body.date,
-                    slotTimeStart:body.slotTimeStart,
-                    slotTimeEnd:body.slotTimeEnd
+                    "id": id,
+                    "tableNumber":body.tableNumber,
+                    "clientName":body.clientName,
+                    "phoneNumber":body.phoneNumber,
+                    "date":body.date,
+                    "slotTimeStart":body.slotTimeStart,
+                    "slotTimeEnd":body.slotTimeEnd
                 }
             };
             await dynamoDB.put(params).promise();
